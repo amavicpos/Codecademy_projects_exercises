e21e9dc68ea5ad08b9b759c8d616b1f05c83bccc
amavicpos <100805638+amavicpos@users.noreply.github.com>
2022-03-15 22:32:52 +0100

    Connect Four game

--- ConnectFour.py
+++ ConnectFour.py
@@ -1,1 +1,17 @@
 # ConnectFour.py
+for i in range(6):
+    position = input("What position do you want to put your piece in? 1 2 3 4 5 6 7")
+    if position == 1:
+        print("x o o o o o o")
+    if position == 2:
+        print("o x o o o o o")
+    if position == 3:
+        print("o o x o o o o")
+    if position == 4:
+        print("o o o x o o o")
+    if position == 5:
+        print("o o o o x o o")
+    if position == 6:
+        print("o o o o o x o")
+    if position == 7:
+        print("o o o o o o x")
