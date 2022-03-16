94fa79bd8eaa21fd6f5808bb5331f987757d1e25
amavicpos <100805638+amavicpos@users.noreply.github.com>
2022-03-16 23:09:03 +0100

    Simple version of game finished

--- ConnectFour.py
+++ ConnectFour.py
@@ -1,17 +1,32 @@
 # ConnectFour.py
-for i in range(6):
-    position = input("What position do you want to put your piece in? 1 2 3 4 5 6 7")
-    if position == 1:
-        print("x o o o o o o")
-    if position == 2:
-        print("o x o o o o o")
-    if position == 3:
-        print("o o x o o o o")
-    if position == 4:
-        print("o o o x o o o")
-    if position == 5:
-        print("o o o o x o o")
-    if position == 6:
-        print("o o o o o x o")
-    if position == 7:
-        print("o o o o o o x")
+import numpy as np
+
+table = np.array([["-", "-", "-", "-", "-", "-", "-"], ["-", "-", "-", "-", "-", "-", "-"], ["-", "-", "-", "-", "-", "-", "-"], ["-", "-", "-", "-", "-", "-", "-"], ["-", "-", "-", "-", "-", "-", "-"], ["-", "-", "-", "-", "-", "-", "-"]])
+rows_table = np.size(table, axis = 0)
+columns_table = np.size(table, axis = 1)
+
+while True:
+    continue_or_not = input("Did someone win?: Answer with \"Yes\" or \"No\" ")
+    if continue_or_not.lower() == "yes":
+        break
+    if continue_or_not.lower() == "no":
+        pass
+    else:
+        print("Answer not valid. Please answer with \"Yes\" or \"No\"")
+        continue
+
+    position1 = int(input("Player 1: Drop your piece in the desired position: 1 2 3 4 5 6 7")) - 1
+    row_position1 = np.where(table[:, position1] == "-")[0][-1]
+    table[row_position1, position1] = "x"
+
+    for x in range(rows_table):
+        print(" ".join(table[x]))
+
+    position2 = int(input("Player 2: Drop your piece in the desired position: 1 2 3 4 5 6 7")) - 1
+    row_position2 = np.where(table[:, position2] == "-")[0][-1]
+    table[row_position2, position2] = "o"
+
+    for x in range(rows_table):
+        print(" ".join(table[x]))
+
+print("Game ended!")
